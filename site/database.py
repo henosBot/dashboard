dc759d2c7eafdf046e06e8f08b239db1db4e8d51
henosBot <spi0003@boxhillhs.vic.edu.au>
2021-02-01 22:31:36 +1100

    some config

--- site/database.py
+++ site/database.py
@@ -1,1 +1,5 @@
+from google.cloud import firestore
+import os
 
+os.chdir('C:\\Users\\SPI0003\\OneDrive - boxhillhs.vic.edu.au\\Home\\Coding\\bot\\dashboard\\dashboard\\site')
+db = firestore.Client.from_service_account_json("firestore.json")
