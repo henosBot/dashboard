70ba92b15f575ad530c1c2f099a4f9089b6356d6
henos <78285334+henosBot@users.noreply.github.com>
2021-02-02 10:53:36 +1100

    Update site.py

--- site/site.py
+++ site/site.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import database as db
 
-st.set_page_config(
-    page_title='henos bot',
-    page_icon='https://imgur.com/a/urHOoK3',
-    initial_sidebar_state='collapsed',
-)
+def start():
+    st.set_page_config(
+        page_title='henos bot',
+        page_icon='https://imgur.com/a/urHOoK3',
+        initial_sidebar_state='collapsed',
+    )
 
-st.text('This website is under development, please wait until complete')
+    st.text('This website is under development, please wait until complete')
